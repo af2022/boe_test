9bfd16e08d4384098279a21d898f0c24138fd3ba
personal <afazil2021@gmail.com>
2021-09-13 21:17:59 +0100

    created test script, uploaded data and created R project file for ease

---
+++ boe_test_script.R
@@ -0,0 +1,17 @@
+# Bank of England Test Answers 
+# Created on 13/09/2021 
+# Created by: Abbas Fazil 
+
+
+# Load Packages -----------------------------------------------------------
+
+# load all required libraries 
+
+library(readxl) # for reading in excel files
+library(dplyr) # for data cleaning 
+
+
+# Load Data ---------------------------------------------------------------
+
+# load both data files into R - only load the sheets that we need. 
+# 
